25bacf20a0feef6fc930084b637e3ab7919304c3
KRakeesh04 <kanesrakeshan@gmail.com>
2025-07-27 03:52:42 +0530

    add Lab1.2-1.3

--- week-01/Lab1/Lab1-2-3/chain.c
+++ week-01/Lab1/Lab1-2-3/chain.c
@@ -5,50 +5,149 @@
 #include "chain.h" // do not modify this file
 
 // put your function prototypes for additional helper functions below:
+void mat_math_operation(matrix *mat, int num, char operator);
+int powerOf(int num, int times);
 
 
 // implementation
 matrix *create_matrix(int num_rows, int num_cols)
 {   
-    // TODO
-    return NULL;
+    matrix *mat = malloc(sizeof(matrix));
+    mat->num_cols = num_cols;
+    mat->num_rows = num_rows;
+    int **data = malloc(num_rows*sizeof(int *));
+    for (int i = 0; i < num_rows; i++)
+    {
+        data[i] = malloc(num_cols*sizeof(int));
+        for (int j = 0; j < num_cols; j++)
+        {
+            data[i][j] = 0;
+        } 
+    }
+    mat->data = data;
+    return mat;
 }
 
 void add_row(matrix *mat, int *row)
 {
-    // TODO
+    int col_count = mat->num_cols;
+    int row_count = mat->num_rows;
+    int **data = mat->data;
+    int **tmp = realloc(data,(row_count+1)*sizeof(int *));
+    if (!tmp) { perror("realloc"); exit(1); }
+    data = tmp;
+
+    data[row_count+1] = malloc(col_count*sizeof(int));
+    if (!data[row_count]) { perror("malloc"); exit(1); }
+    for (int i = 0; i < col_count; i++)
+    {
+        data[row_count][i] = row[i];
+    }
+    mat->data = data;
+    mat->num_rows = row_count + 1;
+
+    
 }
 
 void add_col(matrix *mat, int *col)
 {
-    // TODO
+    int col_count = mat->num_cols;
+    int row_count = mat->num_rows;
+    int **data = mat->data;
+    for (int i = 0; i < row_count; i++)
+    {
+        int *tmp = realloc(data[i], (col_count+1)*sizeof(int));
+        if (!tmp) { perror("realloc"); exit(1); }
+        data[i] = tmp;
+        data[i][col_count] = col[i];
+    }
+    mat->data = data;
+    mat->num_cols = col_count + 1;
 }
 
 void increment(matrix *mat, int num)
 {
-    // TODO
+    mat_math_operation(mat, num, '+');
 }
 
 void scalar_multiply(matrix *mat, int num)
 {
-    // TODO
+    mat_math_operation(mat, num, '*');
 }
 
 void scalar_divide(matrix *mat, int num)
 {
-    // TODO
+    if (num == 0)
+    {
+        return;
+    }
+    mat_math_operation(mat, num, '/');
 }
 
 void scalar_power(matrix *mat, int num)
 {
-    // TODO
+    mat_math_operation(mat, num, '^');
 }
 
 void delete_matrix(matrix *mat)
 {
-    // TODO
+    int rows = mat->num_rows;
+    for (int i = 0; i < rows; i++)
+    {
+        free(mat->data[i]);
+    }
+    free(mat->data);
+    free(mat);
 }
 
+void mat_math_operation(matrix *mat, int num, char operator)
+{
+    int col_count = mat->num_cols;
+    int row_count = mat->num_rows;
+    int **data = mat->data;
+    for (int i = 0; i < row_count; i++)
+    {
+        for (int j = 0; j < col_count; j++)
+        {
+            switch (operator)
+            {
+            case '+':
+                data[i][j] += num;
+                break;
+            
+            case '-':
+                data[i][j] -= num;
+                break;
+            
+            case '*':
+                data[i][j] *= num;
+                break;
+            
+            case '/':
+                data[i][j] /= num;
+                break;
+            
+            case '^':
+                data[i][j] = powerOf(data[i][j], num);
+                break;
+            
+            default:
+                break;
+            }
+        }
+    }
+    mat->data = data;
+}
+
+int powerOf(int num, int times)
+{
+    int result = 1;
+    for (int i = 0; i < times; i++)
+    {
+        result *= num;
+    }
+    return result;   
+}
 
 /*
     DO NOT MODIFY BELOW
@@ -71,3 +170,106 @@ void print_matrix(matrix *mat)
 }
 
 //Add the rest of the functions needed for the chain below
+void insert_node_before(chain *chn, int index, matrix *mat)
+{
+    node *n = malloc(sizeof(node));
+	n->mat = mat;
+	if (chn->head == NULL) 
+	{
+		chn->head = n;
+		n->next = n;
+		n->prev = n;
+	}
+	else
+	{
+		node *temp = chn->head;
+		if (index >= 0) 
+		{
+			for (int i = 0; i < index; i++)
+			{
+				temp = temp->next;
+			}
+		} else 
+		{
+			for (int i = 0; i < -index; i++)
+			{
+				temp = temp->prev;
+			}
+		}
+
+		n->prev = temp->prev;
+		n->next = temp;
+		temp->prev->next = n;
+		temp->prev = n;
+    }
+}
+
+void insert_node_after(chain *chn, int index, matrix *mat)
+{
+	insert_node_before(chn, index + 1, mat);
+}
+
+void delete_node(chain *chn, int index)
+{
+    if (!chn->head) return;
+	node *temp = chn->head;
+	if (index > 0)
+	{
+		for (int i = 0; i < index; i++)
+		{
+			temp = temp->next;
+		}
+	}
+	else if (index < 0)
+	{
+		for (int i = 0; i < abs(index); i++)
+		{
+			temp = temp->prev;
+		}
+		
+	}
+
+    if (temp->next == temp) 
+	{
+        chn->head = NULL;
+    } else {
+        if (temp == chn->head)
+		{
+            chn->head = temp->next;
+		}
+        temp->prev->next = temp->next;
+        temp->next->prev = temp->prev;
+    }
+
+    int **data = temp->mat->data;
+    int rows = temp->mat->num_rows;
+    for (int i = 0; i < rows; i++)
+    {
+        free(data[i]);
+    }
+    free(data);
+    free(temp->mat);
+    free(temp);
+}
+
+void delete_list(chain *chn)
+{
+    if (!chn->head) return;
+
+    node *temp = chn->head;
+    node *next_node;
+    do {
+        next_node = temp->next;
+        int **data = temp->mat->data;
+        int rows = temp->mat->num_rows;
+        for (int i = 0; i < rows; i++)
+        {
+            free(data[i]);
+        }
+        free(data);
+        free(temp->mat);
+        free(temp);
+        temp = next_node;
+    } while (temp != chn->head);
+	chn->head = NULL;
+}
--- week-01/Lab1/Lab1-2-3/lab1-2-3.c
+++ week-01/Lab1/Lab1-2-3/lab1-2-3.c
@@ -5,10 +5,23 @@
 // user-defined header files
 #include "chain.h"
 
+// macros
+#define TRUE 1
+
+#define CREATE_MATRIX 1
+#define ADD_ROW 2
+#define ADD_COLUMN 3
+#define INCREMENT 4
+#define SCALAR_MULTIPLY 5
+#define SCALAR_DiVIDE 6
+#define SCALAR_POWER 7
+#define DELETE_MATRIX 8
+
 // function prototypes
 void print_chain(chain * chn); 
 void run(chain * chn);
-
+matrix *get_input_initial_mat(int rows);
+int *get_input_elements();
 
 int main()
 {
@@ -23,12 +36,144 @@ int main()
 // parse the input
 void run(chain *chn) 
 {
-	
+	int rows;
+    scanf("%d", &rows);
+    matrix *mat = get_input_initial_mat(rows);
+    // chain *chn = malloc(sizeof(chain));
+    insert_node_before(chn, 0, mat);
+
+    while (TRUE)
+    {
+        int opcode;
+        scanf("%d", &opcode);
+        if (opcode == 0)
+        {
+            return;
+        }
+
+        switch (opcode)
+        {
+        case CREATE_MATRIX:
+            int rows, cols;
+            scanf("%d %d", &rows, &cols);
+            matrix *new_mat = create_matrix(rows, cols);
+            insert_node_before(chn, 0, new_mat);
+            break;
+        
+        case ADD_ROW:
+            int *row = get_input_elements();
+            add_row(mat,row);
+            insert_node_before(chn, 0, mat);
+            break;
+        
+        case ADD_COLUMN:
+            int *col = get_input_elements();
+            add_col(mat,col);
+            insert_node_before(chn, 0, mat);
+            break;
+        
+        case INCREMENT:
+            int incre_num;
+            scanf("%d", &incre_num);
+            increment(mat, incre_num);
+            insert_node_before(chn, 0, mat);
+            break;
+        
+        case SCALAR_MULTIPLY:
+            int mul_num;
+            scanf("%d", &mul_num);
+            scalar_multiply(mat, mul_num);
+            insert_node_before(chn, 0, mat);
+            break;
+        
+        case SCALAR_DiVIDE:
+            int div_num;
+            scanf("%d", &div_num);
+            scalar_divide(mat, div_num);
+            insert_node_before(chn, 0, mat);
+            break;
+        
+        case SCALAR_POWER:
+            int pow_num;
+            scanf("%d", &pow_num);
+            scalar_power(mat, pow_num);
+            insert_node_before(chn, 0, mat);
+            break;
+            
+        case DELETE_MATRIX:
+            delete_matrix(mat);
+            insert_node_before(chn, 0, mat);
+            break;
+        
+        default:
+            return;
+        }
+
+    }
+
 }
 
 //Print the chain
 void print_chain(chain * chn)
 {
-   
+    node *temp = chn->head;
+    do
+    {
+        print_matrix(temp->mat);
+        temp = temp->next;
+    } while (temp != chn->head);
+    
+    delete_list(chn);
+}
+
+matrix *get_input_initial_mat(int rows)
+{
+    matrix *mat = malloc(sizeof(matrix));
+    int cols;
+    int **data = malloc(rows*sizeof(int *));
+    for (int i = 0; i < rows; i++)
+    {
+        data[i] = NULL;
+        int j = 0;
+        while (TRUE)
+        {
+            int tmp;
+            if (scanf("%d", &tmp) != 1) 
+            {
+                break;
+            }
+            int *new_row = realloc(data[i], (j+1) * sizeof(int));
+            if (!new_row) { perror("realloc"); exit(1); }
+            data[i] = new_row;
+
+            data[i][j++] = tmp;
+        }
+        cols = j;
+    }
+    mat->data = data;
+    mat->num_rows = rows;
+    mat->num_cols = cols;
+
+    return mat;
+}
+
+int *get_input_elements()
+{
+    int *elem_arr = NULL;
+    int j = 0;
+    while (TRUE)
+    {
+        int tmp;
+        if (scanf("%d", &tmp) != 1) 
+        {
+            break;
+        }
+        int *new = realloc(elem_arr, (j+1) * sizeof(int));
+        if (!new) { perror("realloc"); exit(1); }
+        elem_arr = new;
+
+        elem_arr[j++] = tmp;
+    }
+    return elem_arr;
 }
 
